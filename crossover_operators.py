de7dec11734d5c053c21e6b17666031d208becbe
josemnmatos <josemnmatos@gmail.com>
2025-04-15 16:52:21 +0100

    Uniform crossover

--- crossover_operators.py
+++ crossover_operators.py
@@ -18,3 +18,22 @@ def one_point_crossover(parent1, parent2):
     offspring = offspring.reshape(parent1.shape)
 
     return offspring
+
+
+def uniform_crossover(parent1, parent2):
+    # (5,5) -> vector
+    parent1_vec = parent1.flatten()
+    parent2_vec = parent2.flatten()
+
+    offspring = np.zeros_like(parent1_vec)
+
+    for i in range(len(parent1_vec)):
+        if random.random() < 0.5:
+            offspring[i] = parent1_vec[i]
+        else:
+            offspring[i] = parent2_vec[i]
+
+    # vector -> (5,5)
+    offspring = offspring.reshape(parent1.shape)
+
+    return offspring
