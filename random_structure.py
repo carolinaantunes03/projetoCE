e2ceef87445ac51d4d5355e707bb7dd6d003ad28
josemnmatos <josemnmatos@gmail.com>
2025-04-18 22:51:24 +0100

    Changes

--- random_structure.py
+++ random_structure.py
@@ -1,4 +1,3 @@
-from genetic_programming import (ramped_half_and_half_init_pop, GPTree)
 import json
 import numpy as np
 import random
@@ -7,6 +6,7 @@ import gymnasium as gym
 from evogym.envs import *
 import torch
 import time
+import multiprocessing
 
 from evogym import (
     EvoWorld,
@@ -16,8 +16,9 @@ from evogym import (
     get_full_connectivity,
     is_connected,
 )
-from crossover_operators import one_point_crossover
-from mutation_operators import flip_mutation
+from crossover_operators import one_point_crossover, uniform_crossover
+from genetic_programming import GPTree, ramped_half_and_half_init_pop
+from mutation_operators import flip_mutation, swap_mutation
 import utils
 from fixed_controllers import *
 import time
@@ -26,16 +27,19 @@ import matplotlib.pyplot as plt
 
 
 # ---- PARAMETERS ----
-NUM_GENERATIONS = 25  # Number of generations to evolve
+NUM_GENERATIONS = 250  # Number of generations to evolve
 MIN_GRID_SIZE = (5, 5)  # Minimum size of the robot grid
 MAX_GRID_SIZE = (5, 5)  # Maximum size of the robot grid
 STEPS = 500
-POPULATION_SIZE = 7  # Number of robots per generation
-MUTATION_RATE = 0.05  # Probability of mutation
+POPULATION_SIZE = 20  # Number of robots per generation
+MUTATION_RATE = 0.1  # Probability of mutation
 
-TOURNAMENT_SIZE = 2  # Number of individuals in the tournament for selection
+TOURNAMENT_SIZE = 5  # Number of individuals in the tournament for selection
 ELITISM = True  # Whether to use elitism or not
-ELITE_SIZE = 1  # Number of elite individuals to carry over to the next generation
+ELITE_SIZE = 2  # Number of elite individuals to carry over to the next generation
+
+# -- Sim ---
+MULTIPROCESSING = True  # Whether to use multiprocessing or not
 
 
 # ---- TESTING SETTINGS ----
@@ -51,7 +55,7 @@ CONTROLLERS = {
 }  # we should choose only ONE but we can test all
 
 SCENARIO = "Walker-v0"
-CONTROLLER = hopping_motion  # fixed controller
+CONTROLLER = alternating_gait  # fixed controller
 
 # ---- VOXEL TYPES ----
 VOXEL_TYPES = [0, 1, 2, 3, 4]
@@ -63,7 +67,7 @@ def evaluate_fitness(robot_structure, view=False):
     and returns a fitness score based on how well the robot moves"""
     """O fitness score reflete o quão longe o robot se consegue deslocar"""
     if not is_connected(robot_structure):
-        return 0.0
+        return -15.0, 0
 
     try:
         connectivity = get_full_connectivity(
@@ -81,10 +85,13 @@ def evaluate_fitness(robot_structure, view=False):
         viewer = EvoViewer(sim)
         viewer.track_objects("robot")
         t_reward = 0
-        max_velocity = 0.0
+        t_velocity_x = 0.0
+        t_velocity_y = 0.0
         max_step_reward = 0.0
         action_size = sim.get_dim_action_space(
             "robot")  # Get correct action size
+
+        t = 0
         for t in range(STEPS):
 
             # Update actuation before stepping
@@ -92,7 +99,25 @@ def evaluate_fitness(robot_structure, view=False):
             if view:
                 viewer.render("screen")
             ob, reward, terminated, truncated, info = env.step(actuation)
+
+            time = sim.get_time()
+
+            vel = sim.object_vel_at_time(time, "robot")
+
             t_reward += reward
+
+            # vel.shape = (2,n) velocities in x and y direction for each voxel
+
+            # From paper:
+            # let vo be a vector of length 2 that represents the velocity of the center of mass of an object
+            # o in the simulation at time t. vox and voy denote the x and y components of this vector, respectively. vo
+            # is computed by averaging the velocities of all the point-masses that make up object o at time t
+
+            # Average velocity of the robot at time t
+            vel_at_t = np.mean(vel, axis=1)
+
+            t_velocity_x += vel_at_t[0]
+            t_velocity_y += vel_at_t[1]
 
             max_step_reward = max(max_step_reward, reward)
 
@@ -103,12 +128,23 @@ def evaluate_fitness(robot_structure, view=False):
         viewer.close()
         env.close()
 
-        fitness_val = t_reward + 10 * max_step_reward
+        total_time = sim.get_time()
+
+        avg_velocity_x = t_velocity_x / t
+        avg_velocity_y = t_velocity_y / t
+
+        primary_obj_reward = reward
+
+        secondary_obj_reward = avg_velocity_x
+
+        time_penalty_factor = 0.05
+
+        fitness_val = t_reward / (1 + time_penalty_factor * total_time)
 
         return fitness_val, t_reward  # return fitness and reward
 
     except (ValueError, IndexError) as e:
-        return 0.0
+        return -15.0, 0  # Return a low fitness score if an error occurs
 
 
 def evaluate_population_fitness(population) -> tuple:
@@ -116,10 +152,42 @@ def evaluate_population_fitness(population) -> tuple:
     fitness_scores = []
     rewards = []
 
-    for robot in population:
-        fitness_score, reward = evaluate_fitness(robot)
-        fitness_scores.append(fitness_score)
-        rewards.append(reward)
+    if not MULTIPROCESSING:
+        # Sequential execution if multiprocessing is not enabled
+        for robot in population:
+            fitness_score, reward = evaluate_fitness(robot)
+            fitness_scores.append(fitness_score)
+            rewards.append(reward)
+        return fitness_scores, rewards
+
+    # Use more than one process, e.g., the number of available CPU cores
+    # Be mindful of resource limits and potential overhead
+    # Use number of robots or CPU cores, whichever is smaller
+    num_processes = min(len(population), os.cpu_count())
+    if num_processes > 1:  # Only use multiprocessing if beneficial
+        try:
+            # Ensure the main script execution is guarded by if __name__ == "__main__":
+            # This is crucial for multiprocessing on Windows.
+            with multiprocessing.Pool(processes=8) as pool:
+                results = pool.map(evaluate_fitness, population)
+
+            for fitness_score, reward in results:
+                fitness_scores.append(fitness_score)
+                rewards.append(reward)
+        except Exception as e:
+            print(
+                f"Multiprocessing failed: {e}. Falling back to sequential execution.")
+            # Fallback to sequential execution if multiprocessing fails
+            for robot in population:
+                fitness_score, reward = evaluate_fitness(robot)
+                fitness_scores.append(fitness_score)
+                rewards.append(reward)
+    else:
+        # Execute sequentially if only one process is needed or available
+        for robot in population:
+            fitness_score, reward = evaluate_fitness(robot)
+            fitness_scores.append(fitness_score)
+            rewards.append(reward)
 
     return fitness_scores, rewards
 
@@ -136,8 +204,7 @@ def create_random_robot():
     return random_robot
 
 
-def valid_robot():
-
+def generate_valid_robot():
     while True:
         robot = create_random_robot()
         if is_connected(robot):
@@ -175,12 +242,7 @@ def random_search(population_size=POPULATION_SIZE):
         best_gen_reward = -float("inf")
 
         population = [create_random_robot() for _ in range(population_size)]
-        fitness_scores = []
-        rewards = []
-        for robot in population:
-            fitness_score, reward = evaluate_fitness(robot)
-            fitness_scores.append(fitness_score)
-            rewards.append(reward)
+        fitness_scores, rewards = evaluate_population_fitness(population)
 
         max_fitness_idx = np.argmax(fitness_scores)
         robot = population[max_fitness_idx]
@@ -226,7 +288,8 @@ def random_search(population_size=POPULATION_SIZE):
 
 def evolutionary_algorithm(elitism=ELITISM):
 
-    population = [valid_robot() for individual in range(POPULATION_SIZE)]
+    population = [generate_valid_robot()
+                  for individual in range(POPULATION_SIZE)]
     # population = [create_random_robot() for individual in range(POPULATION_SIZE)]
     fitness_scores, reward_scores = evaluate_population_fitness(population)
 
@@ -274,7 +337,7 @@ def evolutionary_algorithm(elitism=ELITISM):
 
             # If offspring is disconnected, discard it and generate a valid robot
             if not is_connected(offspring):
-                offspring = valid_robot()
+                offspring = generate_valid_robot()
                 # offspring = create_random_robot()
 
             new_population.append(offspring)
@@ -322,12 +385,13 @@ def evolutionary_algorithm(elitism=ELITISM):
 # ----------Genetic Programming - Generation Expression Trees--------------------------------------------------------
 
 
-MAX_GP_TREE_DEPTH = 5
+MIN_GP_TREE_DEPTH = 1
+MAX_GP_TREE_DEPTH = 4
 
 
 def standard_gp(elitism=ELITISM):
-    population: List[GPTree] = ramped_half_and_half_init_pop(
-        max_depth=MAX_GP_TREE_DEPTH, pop_size=POPULATION_SIZE)
+    population: List[GPTree] = ramped_half_and_half_init_pop(min_depth=MIN_GP_TREE_DEPTH, mutation_rate=MUTATION_RATE, crossover_rate=0.80,
+                                                             max_depth=MAX_GP_TREE_DEPTH, pop_size=POPULATION_SIZE)
 
     robot_pop = [tree.decode_to_robot((5, 5)) for tree in population]
 
@@ -351,6 +415,9 @@ def standard_gp(elitism=ELITISM):
     for generation in range(NUM_GENERATIONS):
         nextgen_pop = []
 
+        best_gen_fitness = -float("inf")
+        best_gen_reward = -float("inf")
+
         # Elitism
         if elitism == True:
             # Get indices sorted by fitness
@@ -365,17 +432,34 @@ def standard_gp(elitism=ELITISM):
             # Select parents using tournament selection
             parent1 = tournament_selection(population, fitness_scores)
             parent2 = tournament_selection(population, fitness_scores)
-            parent1.crossover(parent2)
-            parent1.mutation()
-            nextgen_pop.append(parent1)
+
+            # Create copies to generate offspring
+            offspring1 = copy.deepcopy(parent1)
+            # Need a copy of parent2 for crossover
+            offspring2 = copy.deepcopy(parent2)
+
+            # Crossover modifies offspring1 in-place using offspring2
+            offspring1.crossover(offspring2)
+            offspring1.mutation()
+
+            nextgen_pop.append(offspring1)
 
         # Generational Strategy?
 
         population = nextgen_pop
+
         robot_pop = [tree.decode_to_robot((5, 5)) for tree in population]
+
+        # --- Add Logging Here ---
+        connected_count = sum(1 for robot in robot_pop if is_connected(robot))
+        disconnected_perc = (1 - (connected_count / POPULATION_SIZE)) * 100
+        print(f"Gen. {generation + 1} Disconnected: {disconnected_perc:.1f}%")
+        # --- End Logging ---
+
         fitness_scores, rewards = evaluate_population_fitness(robot_pop)
         best_fitness_idx = np.argmax(fitness_scores)
         best_reward_idx = np.argmax(rewards)
+
         # Update best fitness and robot if needed
         if fitness_scores[best_fitness_idx] > best_fitness:
             best_fitness = fitness_scores[best_fitness_idx]
@@ -400,8 +484,12 @@ def standard_gp(elitism=ELITISM):
         average_reward_history.append(average_reward)
 
         print(
-            f"Gen. {generation + 1} | Curr.Fit. = {fitness_scores[best_fitness_idx]:.2f} | BestFitGen. = {best_fitness:.2f} | BestFitOvr. = {best_fitness:.2f} | Avg.Fit. = {average_fitness:.2f} | BestRewardGen = {best_reward:.2f} | Avg.Reward = {average_reward:.2f}"
+            # Use best_gen_fitness and best_gen_reward for generation-specific reporting
+            f"Gen. {generation + 1} | Curr.Fit. = {fitness_scores[best_fitness_idx]:.2f} | BestFitGen. = {best_gen_fitness:.2f} | BestFitOvr. = {best_fitness:.2f} | Avg.Fit. = {average_fitness:.2f} | BestRewardGen = {best_gen_reward:.2f} | Avg.Reward = {average_reward:.2f}"
         )
+
+        print(f"Best robot tree:\n {best_robot_tree.print_tree()}")
+        print(f"Best robot structure:\n {best_robot}")
 
     return (
         best_robot,
@@ -412,8 +500,6 @@ def standard_gp(elitism=ELITISM):
         average_reward_history,
     )
 
-# ----------Main Function--------------------------------------------------------
-
 
 def setup_run(seed):
     np.random.seed(seed)
@@ -423,13 +509,16 @@ def setup_run(seed):
 
 # Choose which approach to run:
 if __name__ == "__main__":
+
+    multiprocessing.freeze_support()  # For Windows compatibility
+
     RUN_SEEDS = [6363, 9374, 2003, 198, 2782]
     results_folder = "results/task1"
 
     experiment_info = {
         # ***********************************************************************************
         # Change this to the name of the experiment. Will be used in the folder name.
-        "name": "GP_test",
+        "name": "(0)RandomSearch",
         # ***********************************************************************************
         "repetitions": len(RUN_SEEDS),
         "num_generations": NUM_GENERATIONS,
@@ -498,7 +587,7 @@ if __name__ == "__main__":
             average_fitness_history,
             best_reward_history,
             average_reward_history,
-        ) = standard_gp(elitism=ELITISM)
+        ) = random_search()
         # **********************************************************************
         end_time = time.time()
 
